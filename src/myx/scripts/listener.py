312784b117693ac1b4b71e622a39f8dd876eccb7
jay <springwsxrfv@gmail.com>
2020-12-10 17:22:32 -0500

    meeee

--- src/myx/scripts/listener.py
+++ src/myx/scripts/listener.py
@@ -14,10 +14,13 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("jay_chatter", String, callback)
+    rospy.Subscriber("chatter", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
     listener()
+    rospy.Subscriber("chatter", String, callback)
+
+
--- src/myx/scripts/talker.py
+++ src/myx/scripts/talker.py
@@ -4,11 +4,11 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('jay_chatter', String, queue_size=10)
+    pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "jjjlo world %s" % rospy.get_time()
+        hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
@@ -18,3 +18,4 @@ if __name__ == '__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
+
